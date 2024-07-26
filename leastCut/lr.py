31766c6a1a6ddeeed8fbb4693706263ced9ca9a8
ykamoji <ykamoji@umass.edu>
2024-07-26 19:24:24 +0530

    Interval DP

---
+++ leastCut/lr.py
@@ -0,0 +1,27 @@
+from utils.dputil import putValue, getValue, printDP
+
+rod = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def lr(l, r, usedp=False):
+
+    if l + 1 == r:
+        return 0
+
+    if usedp: getValue(l, r)
+
+    value = 1e9
+    for i in range(l+1, r):
+        value = min(value, rod[r] - rod[l] + lr(l, i, usedp) + lr(i, r, usedp))
+
+    if usedp: putValue(l, r, value)
+
+    return value
+
+
+value = lr(0, len(rod) - 1, usedp=True)
+
+print(f"Best minimum cut = {value}")
+
+# printDP()
+
