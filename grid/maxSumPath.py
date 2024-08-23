aa7e16da5e64f184548f781fe37d552bc01534e6
ykamoji <ykamoji@umass.edu>
2024-08-23 12:48:17 +0530

    Added iterative code

--- grid/maxSumPath.py
+++ grid/maxSumPath.py
@@ -1,4 +1,4 @@
-from utils.dputil import getValue, putValue, getDP
+from utils.dputil import getValue, putValue, getDP, printDP
 
 paths = [
     [2, 3, 5, 1],
@@ -9,21 +9,42 @@ paths = [
 
 path_walked = []
 
-def walk(m, n, usedp=False):
 
-    if m == len(paths[0]) or n == len(paths):
-        return 0
+def withRec():
+    def walk(m, n, usedp=False):
 
-    if usedp: getValue(m, n)
+        if m == len(paths[0]) or n == len(paths):
+            return 0
 
-    hor, ver = walk(m + 1, n), walk(m, n + 1)
+        if usedp: getValue(m, n)
 
-    sum = max(hor, ver) + paths[m][n]
+        hor, ver = walk(m + 1, n), walk(m, n + 1)
 
-    if usedp: putValue(m, n, sum)
+        sum = max(hor, ver) + paths[m][n]
 
-    return sum
+        if usedp: putValue(m, n, sum)
+
+        return sum
+
+    print(f"sum = {walk(0,0, True)}")
+    print(getDP())
 
 
-print(f"sum = {walk(0,0, True)}")
-print(getDP())
+def withIteration():
+
+    rows = len(paths)
+    cols = len(paths[0])
+
+    def walk():
+        for i in range(rows, -1, -1):
+            for j in range(cols, -1, -1):
+                if i == rows or j == cols:
+                    putValue(i, j, 0)
+                else:
+                    putValue(i, j, max(getValue(i + 1, j), getValue(i, j + 1)) + paths[i][j])
+
+    walk()
+    print(f"sum = {getValue(0, 0)}")
+
+# withRec()
+withIteration()
