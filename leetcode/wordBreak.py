38f8b2b0c97c2931cb044434bca40ecc0da61818
ykamoji <ykamoji@umass.edu>
2024-09-13 23:34:56 -0400

    Leedcode ##139

---
+++ leetcode/wordBreak.py
@@ -0,0 +1,29 @@
+s = "catsandogcat"
+wordDict = ["cats","dog","sand","and","cat","an"]
+wordDict_len = len(wordDict)
+
+dp = [[-1 for _ in range(len(s)+1)] for _ in range(len(s)+1)]
+s_len = len(s)
+
+
+def combinationAvailable(start):
+
+    if start == 0:
+        return True
+
+    if dp[start] != -1:
+        return dp[start]
+
+    result = False
+    for word in wordDict:
+
+        word_len = len(word)
+
+        if s[start: start + word_len] == word:
+            result = result or combinationAvailable(start + word_len)
+
+    dp[start] = result
+
+    return result
+
+print(f"{combinationAvailable(0)}")
