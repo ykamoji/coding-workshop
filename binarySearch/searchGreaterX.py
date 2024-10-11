540688addd63b9c926cebb1029266c43b65864d7
ykamoji <ykamoji@umass.edu>
2024-10-11 17:03:04 -0400

    binary search bound

---
+++ binarySearch/searchGreaterX.py
@@ -0,0 +1,26 @@
+X = 31
+arr = [1, 5, 10, 31, 31, 33, 34, 35, 100]
+
+
+def searchGreatestX():
+    low = 0
+    high = len(arr)-1
+    ans = len(arr)
+    while low <= high:
+        mid = (low + high)//2
+        if arr[mid] >= X:
+            ans = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return ans
+
+idx = searchGreatestX()
+print(f"First index with >={X} is at index {idx}")
+for index in range(len(arr)):
+    if index == idx:
+        print(f'|{arr[index]}|', end=' ')
+    else:
+        print(arr[index], end=' ')
+
