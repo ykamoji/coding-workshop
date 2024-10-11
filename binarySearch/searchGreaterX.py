158eeb55ccafdf1189ab60f3dd1ef164f5b868c3
ykamoji <ykamoji@umass.edu>
2024-10-11 17:22:14 -0400

    binary search bound

--- binarySearch/searchGreaterX.py
+++ binarySearch/searchGreaterX.py
@@ -2,19 +2,24 @@ X = 31
 arr = [1, 5, 10, 31, 31, 33, 34, 35, 100]
 
 
+def check(mid):
+    return arr[mid] >= X
+
+
 def searchGreatestX():
     low = 0
     high = len(arr)-1
     ans = len(arr)
     while low <= high:
         mid = (low + high)//2
-        if arr[mid] >= X:
+        if check(mid):
             ans = mid
             high = mid - 1
         else:
             low = mid + 1
 
     return ans
+
 
 idx = searchGreatestX()
 print(f"First index with >={X} is at index {idx}")
